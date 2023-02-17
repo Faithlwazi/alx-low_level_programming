b476e243e9239eab4011a83d793725779599e1d6
Faithlwazi <faithlwazikholo@gmail.com>
2023-02-17 01:56:15 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+print("with proper grammar, but the outcome is a piece of a art,\n")
+return (0);
+}
