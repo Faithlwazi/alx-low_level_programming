0cc42d478cc1f351ca41d91c098a5331aa99c354
Faithlwazi <faithlwazikholo@gmail.com>
2023-02-19 10:21:00 -0800

    2-print_alphabet.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#lnclude < stdio.h >
+/**
+ * main - EntrY
+ * Return: Always 0
+ */
+int main (void)
+{
+	printf("size of a char: %i byte(s)\n", sizeof(char));
+	printf("size of an int: %i byte(s)\n", sizeof(int));
+	printf("size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("sze of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
