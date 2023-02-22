4c56cbb9c8ce8647ff0da6cfa58280f74daf7a90
Faithlwazi <faithlwazikholo@gmail.com>
2023-02-22 01:38:51 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- * main - print _putchar
- * is written by mbah
- * Return: 0
+ * main - prints _putchar and a new line
+ * Return: Always 0
  */
-
 int main(void)
 {
 _putchar('_');
@@ -16,5 +14,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
+
 return (0);
 }
