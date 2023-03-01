fdc585dcd009ced6d1066589efe9e8c78fbc26ac
Faithlwazi <faithlwazikholo@gmail.com>
2023-02-28 23:29:20 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,18 @@
-#iclude  < stdio.h >
+#include <stdio.h>
 
 /**
- * main - prints the alphabet
- *
- * Return: Always 0 (success)
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
-char alp[26] = "abcdefghijklmnopqrstuvwxyz" :
-int i
-for (i - 0; i < 26; i++)
 {
-putchar(alp[i]) :
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
 }
-putchar(*\n*) :
-return (0)
+putchar('\n');
+return (0);
+}
